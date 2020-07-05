c6ae26b4209f6480472b59e468d9e62f14a01ac4
Islombek <mislombek27@gmail.com>
2020-07-05 18:46:20 +0500

    ViewSets and Routers features are added

--- profilesapi/profiles/api/urls.py
+++ profilesapi/profiles/api/urls.py
@@ -1,16 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from profiles.api.views import ProfileViewSet
+from profiles.api.views import AvatarUpdateView, ProfileViewSet, ProfileStatusViewSet
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
+router.register(r"status", ProfileStatusViewSet)
+
 
 # profile_list = ProfileViewSet.as_view({"get": "list"})
 # profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
 
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("avatar/", AvatarUpdateView.as_view(), name="avatar-update")
     # path("profiles/", profile_list, name='profile-list'),
     # path("profiles/<int:pk>/", profile_detail, name='profile-detail')
 ]
