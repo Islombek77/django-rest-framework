4c47399cf217e8916b4ed9a109b03632414275ce
Islombek <mislombek27@gmail.com>
2020-07-05 21:53:16 +0500

    Automated tests are written

--- profilesapi/profiles/tests.py
+++ profilesapi/profiles/tests.py
@@ -1,3 +1,73 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from profiles.models import Profile
+from profiles.api.serializers import ProfileSerializer
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        url = "/api/rest-auth/registration/"
+        data = {"username":"testcase",
+                "email":"test@test.com",
+                "password1":"some_strong_password",
+                "password2":"some_strong_password"}
+        response = self.client.post(url, data, fromat='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class ProfileViewSetTestCase(APITestCase):
+
+    list_url = reverse("profile-list")
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="davinci",
+                                             password="some_strong_password")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+    def test_profile_list_authenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_profile_list_un_authenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_profile_detail_retrieve(self):
+        response = self.client.get(reverse("profile-detail", kwargs={"pk":1}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["user"], "davinci")
+
+    def test_profile_update_by_owner(self):
+        response = self.client.put(reverse("profile-detail", kwargs={"pk": 1}),
+                                   {
+                                       "city":"Anchiano",
+                                       "bio":"Renaissance Genius"
+                                   })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content),
+                         {
+                             "id":1,
+                             "user": "davinci",
+                             "bio": "Renaissance Genius",
+                             "city":"Anchiano",
+                             "avatar": None
+                         })
+
+    def test_profile_update_by_random_owner(self):
+        random_user = User.objects.create_user(username="random", password="psw1234567890-=")
+        self.client.force_authenticate(user=random_user)
+        response = self.client.put(reverse("profile-detail", kwargs={"pk": 1}),
+                                   {
+                                       "bio": "hacked!!!"
+                                   })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
